c04de5e0c5d34f66c2060afaa163c4b8b78463a9
belekarvikas-afk <belekarvikas@gmail.com>
2026-08-20 17:44:49 +0530

    ok zale

---
+++ parant_env/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers{
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = "5.0.3"
+    }
+  }
+backend "azurerm" {
+    resource_group_name = "b18g35-common-rg"
+    storage_account_name = "b18g35storageaccount2"
+    container_name = "tfstatefiles"
+    key="dvkiduniya.tfstate"
+  
+}
+
+
+
+}
+
+provider "azurerm" {
+    features{}
+}
