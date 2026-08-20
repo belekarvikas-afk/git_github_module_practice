2a80b57872002bc010b17a9554bd9452818ab683
belekarvikas-afk <belekarvikas@gmail.com>
2026-08-20 17:13:28 +0530

    Add Terraform GitHub Actions pipeline

--- parant_env/main.tf
+++ parant_env/main.tf
@@ -1,4 +1,16 @@
 module "resource_group"{
     source = "../module/01_resources_group"
+   resource_group = var.resource_group
     
+}
+
+module "virtual_network"{
+    source = "../module/02_azurerm_virtual_network"
+    v-net = var.virtual_network
+}
+
+
+module "subnet" {
+    source = "../module/03_azurerm_sub_network"
+    subnet= var.subnet
 }
