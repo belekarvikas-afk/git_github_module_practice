e32f2ad837cc6d9c81c80d16722a679db9772f00
belekarvikas-afk <belekarvikas@gmail.com>
2026-08-20 18:19:23 +0530

    update terraform module pipeline

--- parant_env/main.tf
+++ parant_env/main.tf
@@ -1,17 +1,17 @@
 module "resource_group" {
-  source         = "../../module/01_resources_group"
+  source         = "../module/01_resources_group"
   resource_group = var.resource_group
 
 }
 
 module "virtual_network" {
-  source     = "../../module/02_azurerm_virtual_network"
+  source     = "../module/02_azurerm_virtual_network"
   v-net      = var.virtual_network
   depends_on = [module.resource_group]
 }
 
 module "subnet" {
-  source     = "../../module/03_azurerm_sub_network"
+  source     = "../module/03_azurerm_sub_network"
   subnet     = var.subnet
   depends_on = [module.virtual_network]
 }
