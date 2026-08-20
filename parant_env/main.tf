4a6ecf67490146d05fba4cc3c2ad86c534946bab
belekarvikas-afk <belekarvikas@gmail.com>
2026-08-20 17:47:56 +0530

    good ok zale

--- parant_env/main.tf
+++ parant_env/main.tf
@@ -7,10 +7,12 @@ module "resource_group"{
 module "virtual_network"{
     source = "../module/02_azurerm_virtual_network"
     v-net = var.virtual_network
+    depends_on = [ module.resource_group ]
 }
 
 
 module "subnet" {
     source = "../module/03_azurerm_sub_network"
     subnet= var.subnet
+    depends_on = [ module.virtual_network ]
 }
